5b8049fa695acfe84bc417b46990a9d702037e42
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-08 10:38:00 -0800

    index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
 //fetchMyIP((error, ip) => {
 //  if (error) {
@@ -8,3 +9,11 @@
 //  ip = "curl 'https://api.ipify.org?format=json'";
 //  console.log('It worked! Returned IP:' , ip);
 //});
+
+fetchCoordsByIP('24.71.170.13', (error, data) => {
+  if (error) {
+       console.log("It didn't work!" , error);
+          return;
+   }
+     console.log('It worked! Returned IP:' , data);
+  });
