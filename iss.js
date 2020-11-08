8b0b5b701a5be911f196acd49c310975f2624130
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-08 10:37:40 -0800

    fetch coordinates by IP

--- iss.js
+++ iss.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
 
     if (error) {
@@ -16,5 +16,23 @@ const fetchMyIP = function(callback) {
     callback(null, ip);
   })
 };
-  
-module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://geo.ipify.org/api/v1?apiKey=at_0HeYVeava9Sj81wMlMWdZRdIyjAxn&ipAddress=8.8.8.8`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching coordinates from IP: ${body}`))
+      return;
+    }
+    const ip = JSON.parse(body).ip;
+    const latitude = data.latitude;
+    const longitude = data.longitude;
+    const coordinates = {latitude, longitude};
+    callback(null, coordinates);
+  })
+};
+
+module.exports = {fetchMyIP, fetchCoordsByIP};
