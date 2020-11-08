fed66da5fedc749c1889ca849421e8f3c7e32cb3
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-08 11:31:33 -0800

    index

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-//const { fetchMyIP } = require('./iss');
+const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
-//fetchMyIP((error, ip) => {
-//  if (error) {
-//    console.log("It didn't work!" , error);
-//    return;
-//  }
-//  ip = "curl 'https://api.ipify.org?format=json'";
-//  console.log('It worked! Returned IP:' , ip);
-//});
-
-fetchCoordsByIP('24.71.170.13', (error, data) => {
+fetchMyIP((error, ip) => {
   if (error) {
-       console.log("It didn't work!" , error);
-          return;
-   }
-     console.log('It worked! Returned IP:' , data);
+    console.log("It didn't work!" , error);
+    return;
+  }
+  ip = "curl 'https://api.ipify.org?format=json'";
+  console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP('24.71.170.13', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned IP:' , coordinates);
   });
+
+fetchISSFlyOverTimes((error, flyOverTimes) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Return Fly Over Times:', flyOverTimes);
+});
